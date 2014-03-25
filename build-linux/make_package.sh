7d88f498efc301579d54abe64ac66271039ac309
Anita Rao <anita@pagerduty.com>
2014-03-25 16:40:31 -0700

    Trimmed make_package.sh for integrations package.

--- build-linux/make_package.sh
+++ build-linux/make_package.sh
@@ -60,55 +60,12 @@ echo = cleaning build directories
 rm -fr data target
 mkdir data target
 
-echo = /usr/bin/...
-mkdir -p data/usr/bin
-cp ../bin/pd-send data/usr/bin
-
-echo = /usr/share/pdagent/bin
-mkdir -p data/usr/share/pdagent/bin
-cp ../bin/pdagentd.py data/usr/share/pdagent/bin
-
-echo = /var/...
-mkdir -p data/var/log/pdagent
-mkdir -p data/var/lib/pdagent/db
-mkdir -p data/var/lib/pdagent/outqueue
-
-echo = /etc/...
-mkdir -p data/etc/
-cp ../conf/pdagent.conf data/etc/
-mkdir -p data/etc/init.d
-cp init-script.sh data/etc/init.d/pdagent
-chmod 755 data/etc/init.d/pdagent
-
-if [ "$pkg_type" = "deb" ]; then
-    _PY_SITE_PACKAGES=data/usr/share/pyshared
-else
-    _PY_SITE_PACKAGES=data/usr/lib/python2.6/site-packages
-fi
-
-echo = python modules...
-mkdir -p $_PY_SITE_PACKAGES
-cd ..
-find pdagent -type d -exec mkdir -p build-linux/$_PY_SITE_PACKAGES/{} \;
-find pdagent -type f \( -name "*.py" -o -name "ca_certs.pem" \) \
-    -exec cp {} build-linux/$_PY_SITE_PACKAGES/{} \;
-cd -
-
-if [ "$pkg_type" = "deb" ]; then
-    echo = deb python-support...
-    mkdir -p data/usr/share/python-support
-    _PD_PUBLIC=data/usr/share/python-support/python-pdagent.public
-    echo pyversions=2.6- > $_PD_PUBLIC
-    echo >> $_PD_PUBLIC
-    find $_PY_SITE_PACKAGES -type f -name "*.py" | cut -c 5- >> $_PD_PUBLIC
-    find $_PY_SITE_PACKAGES -type f -name "ca_certs.pem" | cut -c 5- >> $_PD_PUBLIC
-fi
+echo = /usr/share/pdagent-integrations/bin
+mkdir -p data/usr/share/pdagent-integrations/bin
+cp ../bin/pd-zabbix data/usr/share/pdagent-integrations/bin
 
 echo = FPM!
-_FPM_DEPENDS="--depends sudo --depends python"
-if [ "$pkg_type" = "deb" ]; then
-    _FPM_DEPENDS="$_FPM_DEPENDS --depends python-support"
-fi
+_FPM_DEPENDS="--depends pdagent"
 
 _SIGN_OPTS=""
 if [ "$pkg_type" = "rpm" ]; then
@@ -117,9 +74,9 @@ fi
 
 cd target
 
-_DESC="The PagerDuty Agent package
-The PagerDuty Agent is a helper program that you install on your
-monitoring system to integrate your monitoring tools with PagerDuty."
+_DESC="The PagerDuty Agent Integrations package
+The PagerDuty Agent Integrations package contains integrations for various
+monitoring tools with the PagerDuty Agent."
 if [ "$pkg_type" = "deb" ]; then
     _PKG_MAINTAINER="Package Maintainer"
 else
@@ -128,7 +85,7 @@ fi
 _PKG_MAINTAINER="$_PKG_MAINTAINER (PagerDuty, Inc.) <packages@pagerduty.com>"
 fpm -s dir \
     -t $pkg_type \
-    --name "pdagent" \
+    --name "pdagent-integrations" \
     --description "$_DESC" \
     --version "0.6" \
     --architecture all \
@@ -140,10 +97,7 @@ fpm -s dir \
     $_SIGN_OPTS \
     --${pkg_type}-user root \
     --${pkg_type}-group root \
-    --config-files /etc/pdagent.conf \
-    --after-install ../$pkg_type/postinst \
-    --before-remove ../$pkg_type/prerm \
     -C ../data \
-    etc usr var
+    usr
 
 exit 0
